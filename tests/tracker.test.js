ee0c40d60170f94618b0663df86b7f864742db81
Matheus Pereira <tuliocorreamatheus@gmail.com>
2026-02-18 15:12:48 -0300

    test: adiciona teste automatizado para toggle de hábito e ganho de XP

---
+++ tests/tracker.test.js
@@ -0,0 +1,45 @@
+import request from 'supertest'
+import app from '../src/app.js'
+import { users, dailyLogs } from '../src/data/db.js'
+
+beforeEach(() => {
+  users.length = 0
+  dailyLogs.length = 0
+})
+
+let token
+
+beforeEach(async () => {
+  await request(app)
+    .post('/auth/register')
+    .send({
+      name: 'Teste',
+      email: 'teste@email.com',
+      password: '123456'
+    })
+
+  const login = await request(app)
+    .post('/auth/login')
+    .send({
+      email: 'teste@email.com',
+      password: '123456'
+    })
+
+  token = login.body.token
+})
+
+describe('Tracker Endpoints', () => {
+
+  it('Deve adicionar hábito e ganhar XP', async () => {
+    const response = await request(app)
+      .post('/tracker/toggle')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        date: '2026-02-15',
+        type: 'leitura'
+      })
+
+    expect(response.statusCode).toBe(200)
+  })
+
+})
