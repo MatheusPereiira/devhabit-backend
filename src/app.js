bf3be81a41f4cf3ee159421cfd9e3afca1edffdb
Matheus Pereira <tuliocorreamatheus@gmail.com>
2026-02-18 15:13:20 -0300

    fix: registra rotas da aplicação no app para evitar erro 404

--- src/app.js
+++ src/app.js
@@ -1,6 +1,23 @@
 import express from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
+
+import authRoutes from './routes/auth.routes.js'
+import userRoutes from './routes/user.routes.js'
+import trackerRoutes from './routes/tracker.routes.js'
+import statsRoutes from './routes/stats.routes.js'
+
+dotenv.config()
 
 const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.use('/auth', authRoutes)
+app.use('/user', userRoutes)
+app.use('/tracker', trackerRoutes)
+app.use('/stats', statsRoutes)
 
 app.get('/', (req, res) => {
   res.json({ message: 'DevHabit API running...' })
