bfcfc1f1fe142c9276c990f05e1bf3be544b98eb
Matheus Pereira <tuliocorreamatheus@gmail.com>
2026-02-19 11:26:09 -0300

    refactor: padroniza user.controller para uso do middleware global de erros

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,15 +1,6 @@
+import { getUserProfile } from '../services/user.service.js'
+
 export const getMe = (req, res) => {
-  const user = req.user
-
-  const next_level_xp = user.level * 100
-
-  return res.json({
-    name: user.name,
-    level: user.level,
-    current_xp: user.current_xp,
-    next_level_xp,
-    lives: user.lives,
-    streak: user.streak_count,
-    avatar: user.avatar_url
-  })
+  const profile = getUserProfile(req.user)
+  return res.json(profile)
 }
