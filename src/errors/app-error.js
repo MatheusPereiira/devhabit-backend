1f4f8296eeef4ef971da70c40eacc965fe759bb6
Matheus Pereira <tuliocorreamatheus@gmail.com>
2026-02-20 11:06:02 -0300

    refactor: melhora estrutura da classe AppError adicionando status e suporte a múltiplos erros

--- src/errors/app-error.js
+++ src/errors/app-error.js
@@ -1,8 +1,11 @@
 export class AppError extends Error {
-  constructor(message, statusCode = 400) {
+  constructor(message, statusCode = 400, errors = null) {
     super(message)
+
     this.statusCode = statusCode
+    this.status = statusCode >= 500 ? 'error' : 'fail'
     this.isOperational = true
+    this.errors = errors
 
     Error.captureStackTrace(this, this.constructor)
   }
