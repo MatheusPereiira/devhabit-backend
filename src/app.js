8325c2b57b8e26d0efd20782dbdf6b1e3cd1da63
Matheus Pereira <tuliocorreamatheus@gmail.com>
2026-02-20 17:25:43 -0300

    feat: integra rate limit global na aplicação para reforço de segurança

--- src/app.js
+++ src/app.js
@@ -2,27 +2,29 @@ import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
 import helmet from 'helmet'
-
 import authRoutes from './routes/auth.routes.js'
 import userRoutes from './routes/user.routes.js'
 import trackerRoutes from './routes/tracker.routes.js'
 import statsRoutes from './routes/stats.routes.js'
+import { rateLimiter } from './middlewares/rate-limit.middleware.js'
 import { errorMiddleware } from './middlewares/error.middleware.js'
 
 dotenv.config()
-
 const app = express()
 
 app.use(helmet())
-
 app.use(cors())
-
 app.use(express.json())
+
+
+app.use(rateLimiter)
+
 
 app.use('/auth', authRoutes)
 app.use('/user', userRoutes)
 app.use('/tracker', trackerRoutes)
 app.use('/stats', statsRoutes)
+
 
 app.get('/', (req, res) => {
   res.json({ message: 'DevHabit API running...' })
