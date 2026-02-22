db3eb5889d485591732d6cf2c993db684d0871e3
Matheus Pereira <tuliocorreamatheus@gmail.com>
2026-02-22 00:17:34 -0300

    refactor(tracker): padroniza estrutura global de respostas

--- src/controllers/tracker.controller.js
+++ src/controllers/tracker.controller.js
@@ -1,10 +1,11 @@
 import { toggleHabitService } from '../services/tracker.service.js'
+import { successResponse } from '../utils/response.js'
 
 export const toggleHabit = (req, res, next) => {
   try {
     const updatedUser = toggleHabitService(req.user, req.body)
 
-    return res.json({
+    return successResponse(res, 'Habit updated successfully', {
       current_xp: updatedUser.current_xp,
       level: updatedUser.level,
       streak: updatedUser.streak_count
