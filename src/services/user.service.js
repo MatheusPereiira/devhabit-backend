ae3362e283922dfd139b89d2b4c6b1f329359ab0
Matheus Pereira <tuliocorreamatheus@gmail.com>
2026-02-23 10:59:46 -0300

    refatoração: melhorar segurança e legibilidade do getUserProfile

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,13 +1,26 @@
 import { nextLevelXP } from '../utils/level.util.js'
 
 export function getUserProfile(user) {
+  if (!user) {
+    throw new Error('Usuário inválido ao gerar perfil')
+  }
+
+  const {
+    name,
+    level,
+    current_xp,
+    lives,
+    streak_count,
+    avatar_url
+  } = user
+
   return {
-    name: user.name,
-    level: user.level,
-    current_xp: user.current_xp,
-    next_level_xp: nextLevelXP(user.level),
-    lives: user.lives,
-    streak: user.streak_count,
-    avatar: user.avatar_url
+    name,
+    level,
+    current_xp,
+    next_level_xp: nextLevelXP(level),
+    lives,
+    streak: streak_count,
+    avatar: avatar_url
   }
 }
