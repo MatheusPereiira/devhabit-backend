8ad81c068b61caea51a712a3a1db2bdfee4f90ee
Matheus Pereira <tuliocorreamatheus@gmail.com>
2026-02-23 11:08:42 -0300

    refatoração: padronizar resposta do user.controller com successResponse

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,6 +1,16 @@
 import { getUserProfile } from '../services/user.service.js'
+import { successResponse } from '../utils/response.js'
 
-export const getMe = (req, res) => {
-  const profile = getUserProfile(req.user)
-  return res.json(profile)
+export const getMe = (req, res, next) => {
+  try {
+    const profile = getUserProfile(req.user)
+
+    return successResponse(
+      res,
+      'Perfil obtido com sucesso',
+      profile
+    )
+  } catch (err) {
+    return next(err)
+  }
 }
