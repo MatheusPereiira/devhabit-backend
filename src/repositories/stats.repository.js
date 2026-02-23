bcdc06daeb76cdc85af367d2420f264d12b1eac9
Matheus Pereira <tuliocorreamatheus@gmail.com>
2026-02-23 10:52:24 -0300

    refatoração: adaptar stats.repository para PostgreSQL

--- src/repositories/stats.repository.js
+++ src/repositories/stats.repository.js
@@ -1,9 +1,15 @@
-import { dailyLogs } from '../data/db.js'
+import { pool } from '../database/pool.js'
 
-export function findHabitsByUserAndDate(userId, date) {
-  return dailyLogs.filter(
-    log =>
-      log.user_id === userId &&
-      log.date === date
+export async function findHabitsByUserAndDate(userId, date) {
+  const result = await pool.query(
+    `
+    SELECT xp_earned
+    FROM habits
+    WHERE user_id = $1
+      AND date = $2
+    `,
+    [userId, date]
   )
+
+  return result.rows
 }
